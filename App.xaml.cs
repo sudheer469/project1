35f0f011c6fffead4cf397be3dc97d1a575cfea8
sudheer469 <34421426+sudheer469@users.noreply.github.com>
2024-09-04 00:14:50 +0530

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MauiTask.Views;
+using MauiTask.Bootstrap;
+
+namespace MauiTask
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            IOCConfiguration.ConfigureServices();
+            MainPage = new NavigationPage(new LoginView());
+        }
+    }
+}
